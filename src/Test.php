2b6b9c21e360ea7531e1566bce561a7518da00bc
Luis Pastén <luispasten.dev@gmail.com>
2021-09-11 13:50:01 -0500

    sdf

--- src/Test.php
+++ src/Test.php
@@ -5,7 +5,8 @@ namespace CIHispano\Starter;
 class Test{
 
     public function sayHola() {
+        // "url": "file://C:/laragon/www/starter-ci-frontend/"
         return "CIHispano REPO!";
     }
-    
+
 }
